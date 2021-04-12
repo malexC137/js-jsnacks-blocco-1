78973b2a7be7b5531404fad4cf86b7fa1c49111c
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-04-12 12:19:16 +0200

    Ex end

--- Es 2/es2.js
+++ Es 2/es2.js
@@ -1,17 +1,17 @@
 var word1 = prompt("Inserisci una parola");
 var word2 = prompt("Inserisci una seconda parole");
-var longWord
-var shortWord
+var longWord;
+var shortWord;
 
 if (word1.length > word2.length) {
-    longWord = word1
-    shortWord = word2
-    console.log("La parola più lunga è " + word1 + ", mentre la parola più corta è " + word2)
+    longWord = word1;
+    shortWord = word2;
+    console.log("La parola più lunga è " + word1 + ", mentre la parola più corta è " + word2);
     document.getElementById("result").innerHTML = "La parola più lunga è " + word1 + ", mentre la parola più corta è " + word2;
 } else if (word2.length > word1.length) {
-    longWord = word2
-    shortWord = word1
-    console.log("La parola più lunga è " + word2 + ", mentre la parola più corta è " + word1)
+    longWord = word2;
+    shortWord = word1;
+    console.log("La parola più lunga è " + word2 + ", mentre la parola più corta è " + word1);
     document.getElementById("result").innerHTML = "La parola più lunga è " + word2 + ", mentre la parola più corta è " + word1;
 } else {
     console.log("Entrambe le parole hanno la stessa lunghezza");
---
+++ Es 3/es3.js
@@ -0,0 +1,11 @@
+var numbers = 0
+var totNumbers = 0;
+var nQuestions = 10;
+
+for (var i = 0; i < nQuestions; i++) {
+    numbers += parseInt(prompt("Inserisci un numero"));
+}
+
+console.log(numbers)
+
+
