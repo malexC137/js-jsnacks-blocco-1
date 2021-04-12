d692b5342c725018ae4c7b14d6e2241615756d36
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-04-12 10:39:21 +0200

    Ex end

--- Es 2/es2.js
+++ Es 2/es2.js
@@ -7,10 +7,13 @@ if (word1.length > word2.length) {
     longWord = word1
     shortWord = word2
     console.log("La parola più lunga è " + word1 + ", mentre la parola più corta è " + word2)
+    document.getElementById("result").innerHTML = "La parola più lunga è " + word1 + ", mentre la parola più corta è " + word2;
 } else if (word2.length > word1.length) {
     longWord = word2
     shortWord = word1
     console.log("La parola più lunga è " + word2 + ", mentre la parola più corta è " + word1)
+    document.getElementById("result").innerHTML = "La parola più lunga è " + word2 + ", mentre la parola più corta è " + word1;
 } else {
-    console.log("Entrambe le parole hanno la stessa lunghezza")
+    console.log("Entrambe le parole hanno la stessa lunghezza");
+    document.getElementById("result").innerHTML = "Entrambe le parole hanno la stessa lunghezza";
 }
