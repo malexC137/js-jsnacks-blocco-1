02c6d2f74131558f567ae35112a1437c3951fa4e
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-04-12 10:34:22 +0200

    Es done

---
+++ Es 2/es2.js
@@ -0,0 +1,16 @@
+var word1 = prompt("Inserisci una parola");
+var word2 = prompt("Inserisci una seconda parole");
+var longWord
+var shortWord
+
+if (word1.length > word2.length) {
+    longWord = word1
+    shortWord = word2
+    console.log("La parola più lunga è " + word1 + ", mentre la parola più corta è " + word2)
+} else if (word2.length > word1.length) {
+    longWord = word2
+    shortWord = word1
+    console.log("La parola più lunga è " + word2 + ", mentre la parola più corta è " + word1)
+} else {
+    console.log("Entrambe le parole hanno la stessa lunghezza")
+}
